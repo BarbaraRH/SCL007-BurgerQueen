71880ddd8ebcf323ef1bfa26147947391e06a5ad
BarbaraRH <barbararamos@MacBook-Air-de-Barbara.local>
2019-03-14 22:30:23 -0300

    Initial src subtree commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCENvXMNUnsESmDZOFVkAFjOolqnuW6hCQ",
+    authDomain: "pruebaangularfirebase-4e7f7.firebaseapp.com",
+    databaseURL: "https://pruebaangularfirebase-4e7f7.firebaseio.com",
+    projectId: "pruebaangularfirebase-4e7f7",
+    storageBucket: "pruebaangularfirebase-4e7f7.appspot.com",
+    messagingSenderId: "900612529003"
+  }
 };
